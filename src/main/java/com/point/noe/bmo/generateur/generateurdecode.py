2405dded1810afae15d43c061e536cc185421494
marrrryzebwjq <marie-2001-point@hotmail.fr>
2024-03-24 17:03:54 +0100

    Générateur de code : Création d'1 fichier par classe

--- src/main/java/com/point/noe/bmo/generateur/generateurdecode.py
+++ src/main/java/com/point/noe/bmo/generateur/generateurdecode.py
@@ -1,49 +1,69 @@
 fileuml = open("classes.dotuml", 'r')
-filejava = open("javaClass.java", 'w')
-
 
 def isDebutDeClasse(ligne) :
     isClass = False
     hasAccolades = False
+    className = ''
+    isAbstract = False #inutilisé
     
-    words = line.split()
+    words = ligne.split()
     if(len(words)>0) :
-        if(words[0] == 'class' or words[0] == 'abstract' or words[0] == '{') :
+        if(words[0] == 'abstract') :
             isClass = True
             hasAccolades = '{' in line
+            className = words[2]
+            isAbstract = True
+        elif(words[0] == 'class') :
+                isClass = True
+                hasAccolades = '{' in line
+                className = words[1]
+        elif(words[0] == '{') :
+                isClass = True
+                hasAccolades = True
     
-    return isClass, hasAccolades
+    return isClass, hasAccolades, className, isAbstract
 
-
+def createClassFile(className) :
+    return open(className+'.java', 'w')
 
 
 # main
 
 writeLine = False
+isOpen = False
 
 for line in fileuml :
     
-    isClass, hasAccolades = isDebutDeClasse(line)
+    isClass, hasAccolades, className, isAbstract = isDebutDeClasse(line)
     
     if ( not(writeLine) and isClass ) : # Si une classe est définie on écrit tout jusque la fin de la classe
+        
+        writeLine = True                # On va écrire la première ligne :
     
-        writeLine = True                # On écrit la première ligne
-    
-        if line.split()[0] == '{' : #(tabulation)
+        if not(line.split()[0]=='{') :    # On créé d'abord un nouveau fichier.
+            filejava = createClassFile(className)
+            filejava.write('public ')
+            isOpen = True
+        else :
             filejava.write('    ')
             
     
-        for word in line.split() :     # Pour chaque mot de la ligne
-            filejava.write(word + ' ') # on écrit le mot
+        for word in line.split() :     # Pour chaque mot de la ligne,
+            filejava.write(word + ' ') # on écrit le mot,
 
-            if '}' in word :           # on regarde si le mot est un }
-                filejava.write("\n")
+            if '}' in word :           # on regarde si le mot est un },
+                filejava.write("\n")    # si c'est le cas on ferme le fichier.
                 writeLine = False
+                filejava.close()
+                isOpen = False
 
-        filejava.write("\n")           #fin de ligne
+        if isOpen :
+            filejava.write("\n")           #fin de ligne.
         
-        if not(hasAccolades) :
-            writeLine = False
+            if not(hasAccolades) :
+                filejava = False
+                filejava.close()
+                isOpen = False
 
     
 
@@ -52,12 +72,14 @@ for line in fileuml :
         for word in line.split() :
             filejava.write(word + ' ')
 
-            if '}' in word :
+            if '}' in word : #fin du fichier
                 filejava.write("\n")
                 writeLine = False
+                filejava.close()
+                isOpen = False
 
-        filejava.write("\n") #fin de ligne
+        if isOpen :
+            filejava.write("\n") #fin de ligne
         
 
 fileuml.close()
-filejava.close()
